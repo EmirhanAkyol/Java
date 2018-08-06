6a7d203346092b90ad5f23462a29a024a3360d1d
EmirhanAkyol <41941532+EmirhanAkyol@users.noreply.github.com>
2018-08-06 09:42:15 +0300

    Add files via upload

---
+++ calculator/src/com/timbuchalka/Main.java
@@ -0,0 +1,130 @@
+package com.timbuchalka;
+//Kullanıcıdan iki deger alan ve dört islemi belirli sınırlamalarla yapan hesap makinesi projesi;
+import java.util.InputMismatchException;
+import java.util.Scanner;
+public class Main
+{
+    public static void main(String[] args)
+    {
+        double number1 ,number2,result;
+        double numbers[] = new double[2];
+        int menu;
+        do
+        {
+            menu = MenuPrefer(); //ilk konsolda gösterilip ve secimin yaptırılacagı yerdir
+            switch(menu)
+            {
+                case 1:
+                    System.out.println(Messages.welcomeMessage); number1=GetDecimalNumber();
+                    System.out.println(Messages.welcomeMessage2); number2=GetDecimalNumber();
+                    result = Addition(number1,number2);
+                    ResultDisplay(result);
+                    break;
+                case 2:
+                    System.out.println(Messages.welcomeMessage); number1=GetDecimalNumber();
+                    System.out.println(Messages.welcomeMessage2); number2=GetDecimalNumber();
+                    result = Subtraction(number1,number2);
+                    ResultDisplay(result);
+                    break;
+                    case 3:
+                        System.out.println(Messages.welcomeMessage); number1=GetDecimalNumber();
+                        System.out.println(Messages.welcomeMessage2); number2=GetDecimalNumber();
+                result = Multiplication(number1,number2);
+                if (result>9.999999999999){
+                   System.out.println("lütfen sayilari küçült"); }
+                else
+                ResultDisplay(result);
+                break;
+                case 4:
+                    System.out.println(Messages.welcomeMessage); number1=GetDecimalNumber(); //ilk sayıyı ekrandan isteyen çıktı
+                    System.out.println(Messages.welcomeMessage2); number2=GetDecimalNumber();// ikinci sayıyı ekrandan isteyen çıktı
+                    result = Division(number1,number2);// sonuc = sayı1/sayı2
+                    if (number2==0){
+                        System.out.println(Messages.zeromessage);
+                    }
+                    else
+                    ResultDisplay(result); //sonucgoster(sonuc)
+                    break;
+
+                case 5:
+                    break; // break komutlarıyla islem yaptıktan sonra islemi bitir demek istiyoruz.
+                default:
+                    Scanner show = new Scanner(System.in);
+                    System.out.println(Messages.finalmessage);
+                    System.out.println(Messages.getFinalmessage);
+                    show.nextLine();
+                    break;
+            }
+        }
+        while( menu!=5 );
+        System.out.println(Messages.exitmassege);
+    }
+    public static void MenuOpen() //MenuPrefer metodunun içinde çagırılır ve ekranda görünen ilk çıktıdır.
+    {
+        System.out.println(Messages.choosemessage);
+    }
+    /* bu metodda kullanıcıdan bir tam sayı girişi istenir
+    kontrol edildikten sonra int tipinde değilse bir hata mesajı verir ve sizden tekrar seçim yapmanızı ister
+    döngümüz while oldugundan dolayı istenilen tipi girene kadar tekrar eder.
+     */
+    public static int MenuPrefer()
+    {
+        int number;
+        MenuOpen();
+        System.out.println(Messages.choosemessage2);
+        while (true)
+
+            try
+            {
+                Scanner scan = new Scanner(System.in);
+                number = scan.nextInt();
+                return number;
+            }
+            catch (InputMismatchException e)
+            {
+                System.out.println(Messages.errormessage2);
+            }
+        }
+
+    public static double GetDecimalNumber()
+    {
+        double number;
+        while (true)
+        {
+            try
+            {
+                Scanner scan = new Scanner(System.in);
+                number = scan.nextDouble();
+                return number;
+            }
+            catch (InputMismatchException e)
+            {
+                System.out.println(Messages.errormessage);
+            }
+        }
+    }
+    public static double Addition(double number1,double number2)
+    {
+        return number1+number2;
+    }
+    public static double Subtraction(double number1,double number2)
+    {
+        return number1-number2;
+    }
+    public static double Division(double number1,double number2)
+    {
+        return number1/number2;
+    }
+    public static double Multiplication(double number1,double number2)
+    {
+        return number1*number2;
+    }
+
+    public static void ResultDisplay( double number)
+    {
+        Scanner show = new Scanner(System.in);
+        System.out.println(Messages.resultmessage +number);
+        System.out.println(Messages.againmessage);
+        show.nextLine();
+    }
+}
---
+++ calculator/src/com/timbuchalka/Messages.java
@@ -0,0 +1,24 @@
+package com.timbuchalka;
+
+class Messages {
+
+    public static final String welcomeMessage ="Ilk sayiyi giriniz : "; // case lerden alınan değerleri içeren mesaj bölümü
+    public static final String welcomeMessage2 ="Ikinci sayiyi giriniz : ";
+    public static final String finalmessage ="Lutfen ekrandaki rakamlar arasindan seciminizi yapiniz:";
+    public static final String getFinalmessage =" Tekrar secim yapmak icin enter'a basiniz";
+    public static final String exitmassege =" Cikis yapildi : ";
+    public static final String choosemessage=
+            "#YAPMAK ISTEDIGINIZ ISLEM TURUNU SECINIZ#\n"+
+            "1-Toplama islemİ\n"+
+            "2-Cikarma islemi\n"+
+            "3-Carpma islemi\n"+
+            "4-Bolme islemi\n"+
+            "5-Cikis islemi";
+    public static final String choosemessage2="Lutfen seciminizi yapiniz : ";
+    public static final String errormessage= "Hata! Lutfen double tipinde bir deger giriniz !";
+    public static final String againmessage="Tekrar islem yapabilmek icin lutfen enter'a basın ";
+    public static final String errormessage2= "Hata! Lutfen int tipinde bir deger giriniz !";
+    public static final String resultmessage="Islemin sonucu : ";
+    public static final String zeromessage="Lutfen 0 dan farklı bir sayı giriniz!";
+
+}
